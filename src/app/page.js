e72c3acdc30250b6afbc5d1e0e0184b71ebb8145
AGarciaNY <aldairgarcia360@gmail.com>
2025-07-09 22:50:50 -0400

    started on chat page

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,47 @@
 "use client"
 import Image from "next/image";
 import styles from "./page.module.css";
+import styless from './chat.module.css'
 import SignUpPage from "./components/signuppage/signup";
 import LogInPage from "./components/loginpage/login"
 import { useState } from "react";
 
+
 export default function Home() {
   const [formDisplay, setFormDisplay] = useState("none")
   return (
-    <div className={styles.page}>
-      <h1 className={styles.title}>Waterdrop Chat</h1>
-      <div className={styles.buttonHolder}>
-        <p className={styles.popupbutton} onClick={() => {
-          setFormDisplay("signup")
-        }}>Sign up</p>
-        <p className={styles.popupbutton} onClick={() => {
-          setFormDisplay("login")
-        }}>log in</p>
-      </div>
+    // landing
+    // <div className={styles.page}>
+    //   <h1 className={styles.title}>Waterdrop Chat</h1>
+    //   <div className={styles.buttonHolder}>
+    //     <p className={styles.popupbutton} onClick={() => {
+    //       setFormDisplay("signup")
+    //     }}>Sign up</p>
+    //     <p className={styles.popupbutton} onClick={() => {
+    //       setFormDisplay("login")
+    //     }}>log in</p>
+    //   </div>
 
-      {/* sign up */}
-      <div style={{ display: (formDisplay === "signup" ? "block" : "none") }} >
-        <SignUpPage style={{ display: "none" }} />
-      </div>
-      <div style={{ display: (formDisplay === "login" ? "block" : "none") }} >
-        <LogInPage />
-      </div>
-    </div >
+    //   {/* sign up */}
+    //   <div style={{ display: (formDisplay === "signup" ? "block" : "none") }} >
+    //     <SignUpPage style={{ display: "none" }} />
+    //   </div>
+    //   <div style={{ display: (formDisplay === "login" ? "block" : "none") }} >
+    //     <LogInPage />
+    //   </div>
+    // </div >
+
+
+    // chat
+    <div>
+      {/* side panel */}
+      <div></div>
+
+      {/* messages */}
+      <div></div>
+
+      {/* inpute */}
+      <div></div>
+    </div>
   );
 }
